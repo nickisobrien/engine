fa741146183574465011c5a6c66d96737d2337f6
Nick <hoop.bbb@gmail.com>
2018-06-26 15:23:57 -0700

    changed bg color

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -56,7 +56,7 @@ int main()
 		processInput(window);
 
 		// render
-		glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		modelShader.use();

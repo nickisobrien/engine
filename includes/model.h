926aa0e1df8e5b778fa2e70872446cc5dcfc3971
Nick <hoop.bbb@gmail.com>
2018-06-25 19:47:23 -0700

    mid model processing

--- includes/model.h
+++ includes/model.h
@@ -134,24 +134,25 @@ class Model
             {
                 aiString str;
                 mat->GetTexture(type, i, &str);
+                // check if texture was loaded before and if so, continue to next iteration: skip loading a new texture
                 bool skip = false;
                 for(unsigned int j = 0; j < textures_loaded.size(); j++)
                 {
                     if(std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
                     {
                         textures.push_back(textures_loaded[j]);
-                        skip = true; 
+                        skip = true; // a texture with the same filepath has already been loaded, continue to next one. (optimization)
                         break;
                     }
                 }
                 if(!skip)
                 {   // if texture hasn't been loaded already, load it
                     Texture texture;
-                    texture.id = TextureFromFile(str.C_Str(), directory);
+                    texture.id = TextureFromFile(str.C_Str(), this->directory);
                     texture.type = typeName;
                     texture.path = str.C_Str();
                     textures.push_back(texture);
-                    textures_loaded.push_back(texture); // add to loaded textures
+                    textures_loaded.push_back(texture);  // store it as texture loaded for entire model, to ensure we won't unnecesery load duplicate textures.
                 }
             }
             return textures;

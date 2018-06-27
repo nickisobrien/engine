ed23304d39fe437dd4ae0bdc5649ad797a04fbd7
Nick <hoop.bbb@gmail.com>
2018-06-27 13:32:10 -0700

    depth testing

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -54,7 +54,7 @@ int main()
 		processInput(window);
 
 		// render
-		glClearColor(0.35f, 0.25f, 0.5f, 1.0f);
+		glClearColor(0.5f, 0.8f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		modelShader.use();

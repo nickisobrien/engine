1a62142a9e61489069194c46115bfaafb0a9418d
Nick <hoop.bbb@gmail.com>
2018-07-23 17:15:53 -0700

    early ray setup

--- srcs/player.cpp
+++ srcs/player.cpp
@@ -68,7 +68,7 @@ bool Player::isGrounded()
 	if (z < 0)
 		z = CHUNK_Z + z;
 
-	// cout << "Block:  (" << x << ", " << y << ", " << z << ")" << endl;
+	// cout << "Block:	(" << x << ", " << y << ", " << z << ")" << endl;
 	// cout << "Player: (" << current.x << ", " << current.y << ", " << current.z << ")" << endl;
 	Block *b = getChunk()->getBlock(x,y-1,z);
 	if (b != NULL && (b->isActive()))
@@ -101,33 +101,151 @@ void Player::mouseClickEvent()
 		x = CHUNK_X + x;
 	if (z < 0)
 		z = CHUNK_Z + z;
-	cout << "Block:  (" << x << ", " << y << ", " << z << ")" << endl;
+	cout << "Block:	(" << x << ", " << y << ", " << z << ")" << endl;
+	cout << "Player: (" << current.x << ", " << current.y << ", " << current.z << ")" << endl;
 	cout << "View vector: "<< this->camera.GetViewVector().x << " " <<
 	this->camera.GetViewVector().y << " " << this->camera.GetViewVector().z << endl;
 
-	glm::vec3 currentCheck = this->getPosition();
+	// glm::vec3 current = this->getPosition();
 	glm::vec3 currentView = this->camera.GetViewVector();
-	int ctr = 0;
 
-	while (ctr < 20)
+	// float tMaxX, tMaxY, tMaxZ; // distance to edge of current voxel
+	// int stepX, stepY, stepZ; // -1 or 1 depending on direction
+	// float tDeltaX, tDeltaY, tDeltaZ; // change in
+
+	// int X = (int)floor(current.x) % CHUNK_X;
+	// int Y = floor(current.y);
+	// int Z = (int)floor(current.z) % CHUNK_Z;
+
+	// int stepX = currentView.x >= 0 ? 1 : -1;
+	// int stepY = currentView.y >= 0 ? 1 : -1;
+	// int stepZ = currentView.z >= 0 ? 1 : -1;
+
+	// float next_voxel_boundary_x = (X+stepX);
+	// float next_voxel_boundary_y = (Y+stepY);
+	// float next_voxel_boundary_z = (Z+stepZ);
+
+	// // float tMaxX = (stepX == 1) ? ceil(current.x) - current.x : current.x - floor(current.x);
+	// // float tMaxY = (stepY == 1) ? ceil(current.y) - current.y : current.y - floor(current.y);
+	// // float tMaxZ = (stepZ == 1) ? ceil(current.z) - current.z : current.z - floor(current.z);
+	// float tMaxX = (next_voxel_boundary_x - X)/currentView.x;
+	// float tMaxY = (next_voxel_boundary_y - Y)/currentView.y;
+	// float tMaxZ = (next_voxel_boundary_z - Z)/currentView.z;
+
+
+	// // float tDeltaX = stepX/currentView.x;
+	// // float tDeltaY = stepY/currentView.y;
+	// // float tDeltaZ = stepZ/currentView.z;
+	// double tDeltaX = 1.0f/currentView.x*stepX;
+	// double tDeltaY = 1.0f/currentView.y*stepY;
+	// double tDeltaZ = 1.0f/currentView.z*stepZ;
+
+	float _bin_size = 1.0f;
+
+	// This id of the first/current voxel hit by the ray.
+	// Using floor (round down) is actually very important,
+	// the implicit int-casting will round up for negative numbers.
+	glm::ivec3 current_voxel((int)floor(current.x) % CHUNK_X, floor(current.y), (int)floor(current.z) % CHUNK_Z);
+
+	// Compute normalized ray direction.
+	glm::vec3 ray = this->camera.GetViewVector();
+	ray.x = -ray.x;
+	ray.y = -ray.y;
+	ray.z = -ray.z;
+
+	// In which direction the voxel ids are incremented.
+	int stepX = (ray.x >= 0) ? 1:-1;
+	int stepY = (ray.y >= 0) ? 1:-1;
+	int stepZ = (ray.z >= 0) ? 1:-1;
+
+	// Distance along the ray to the next voxel border from the current position (tMaxX, tMaxY, tMaxZ).
+	float next_voxel_boundary_x = (current_voxel.x+stepX)*_bin_size;
+	float next_voxel_boundary_y = (current_voxel.y+stepY)*_bin_size;
+	float next_voxel_boundary_z = (current_voxel.z+stepZ)*_bin_size;
+
+	// tMaxX, tMaxY, tMaxZ -- distance until next intersection with voxel-border
+	// the value of t at which the ray crosses the first vertical voxel boundary
+	float tMaxX = (next_voxel_boundary_x - current.x) / ray.x;
+	float tMaxY = (next_voxel_boundary_y - current.y) / ray.y;
+	float tMaxZ = (next_voxel_boundary_z - current.z) / ray.z;
+
+	// tDeltaX, tDeltaY, tDeltaZ --
+	// how far along the ray we must move for the horizontal component to equal the width of a voxel
+	// the direction in which we traverse the grid
+	// can only be FLT_MAX if we never go in that direction
+	float tDeltaX = _bin_size/ray.x*stepX;
+	float tDeltaY = _bin_size/ray.y*stepY;
+	float tDeltaZ = _bin_size/ray.z*stepZ;
+
+	cout << "Current_voxel" << current_voxel.x << " " << current_voxel.y << " " << current_voxel.z << endl;
+	cout << "Steps:" << stepX <<" "<<stepY<<" "<<stepZ<<endl;
+	cout << "Next boundary:"<<next_voxel_boundary_x<<" "<<next_voxel_boundary_y<<" "<<next_voxel_boundary_z<<endl;
+	cout << "tMaxs:"<< tMaxX << " " << tMaxY << " " << tMaxZ << endl;
+	cout << "tDeltas:"<<tDeltaX<<" "<<tDeltaY<< " " << tDeltaZ << endl;
+
+
+
+	Block *b = NULL;
+	int ctr = 0;
+	do
 	{
-		cout << "Current check(s): "<< (int)currentCheck.x << " " <<
-		(int)currentCheck.y << " " << (int)currentCheck.z << " floats: " << currentCheck.x << " "
-		<< currentCheck.y << " " << currentCheck.z << endl;
-		
-		// need to implement a way to do checks where i only subtract certain axis's line bresenham
-		if (this->getChunk()->getBlock(round(currentCheck.x),round(currentCheck.y),round(currentCheck.z)) &&
-			this->getChunk()->getBlock(round(currentCheck.x),round(currentCheck.y),round(currentCheck.z))->getType()!=0)
+		if (tMaxX < tMaxY)
 		{
-			this->getChunk()->getBlock(round(currentCheck.x),round(currentCheck.y),round(currentCheck.z))->setType(0);
-			this->getChunk()->update();
-			break;
+			if (tMaxX < tMaxZ)
+			{
+				current_voxel.x += stepX;
+				tMaxX += tDeltaX;
+			}
+			else
+			{
+				current_voxel.z += stepZ;
+				tMaxZ += tDeltaZ;
+			}
 		}
-		currentCheck.x -= currentView.x/10.0f;
-		currentCheck.y -= currentView.y/10.0f;
-		currentCheck.z -= currentView.z/10.0f;
-		ctr++;
+		else
+		{
+			if (tMaxY < tMaxZ)
+			{
+				current_voxel.y += stepY;
+				tMaxY += tDeltaY;
+			}
+			else
+			{
+				current_voxel.z += stepZ;
+				tMaxZ += tDeltaZ;
+			}
+			b = this->getChunk()->getBlock(current_voxel.x,current_voxel.y,current_voxel.z);
+			ctr++;
+		}
+	} while ((!b || b->getType() == 0) && ctr < 20);
+	if (b && b->getType()!=0)
+	{
+		cout << "FOUND " << current_voxel.x << " " << current_voxel.y << " " << current_voxel.z << endl;
+		b->setType(0);
+		this->getChunk()->update();
 	}
+
+
+	// int ctr = 0;
+	// while (ctr < 20)
+	// {
+	// 	cout << "Current check(s): "<< (int)currentCheck.x << " " <<
+	// 	(int)currentCheck.y << " " << (int)currentCheck.z << " floats: " << currentCheck.x << " "
+	// 	<< currentCheck.y << " " << currentCheck.z << endl;
+		
+	// 	// need to implement a way to do checks where i only subtract certain axis's line bresenham
+	// 	if (this->getChunk()->getBlock(round(currentCheck.x),round(currentCheck.y),round(currentCheck.z)) &&
+	// 		this->getChunk()->getBlock(round(currentCheck.x),round(currentCheck.y),round(currentCheck.z))->getType()!=0)
+	// 	{
+	// 		this->getChunk()->getBlock(round(currentCheck.x),round(currentCheck.y),round(currentCheck.z))->setType(0);
+	// 		this->getChunk()->update();
+	// 		break;
+	// 	}
+	// 	currentCheck.x -= currentView.x/10.0f;
+	// 	currentCheck.y -= currentView.y/10.0f;
+	// 	currentCheck.z -= currentView.z/10.0f;
+	// 	ctr++;
+	// }
 
 
 

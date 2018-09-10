92b9a544430dc26d13614c017249dd2f71002b5e
Nick <hoop.bbb@gmail.com>
2018-09-10 11:36:34 -0700

    init

--- srcs/engine.cpp
+++ srcs/engine.cpp
@@ -1,23 +1,10 @@
 #include "engine.h"
-#include "shader.h"
-#include "camera.h"
-#include "mesh.h"
-#include "model.h"
-#include "terrain.h"
-#include "chunk.h"
-#include "player.h"
-#include "textureEngine.h"
-
-#define RENDER_RADIUS 18
 
 float deltaTime = 0.0f;	// Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
 float lastX = WIDTH / 2.0f;
 float lastY = HEIGHT / 2.0f;
 bool firstMouse = true;
-TextureEngine textureEngine;
-Terrain terr;
-Player player(glm::vec3(CHUNK_X/2.0f, (float)CHUNK_Y-30.0f, CHUNK_Z/2.0f), &terr);
 
 int main(void)
 {
@@ -33,7 +20,7 @@ int main(void)
 
 	// glfw window creation
 	// GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Engine", glfwGetPrimaryMonitor(), NULL);
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Engine", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Mod1", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -43,21 +30,9 @@ int main(void)
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // turn on mouse capturing
-	glfwSetCursorPosCallback(window, mouse_callback); // calls mouse_callback every time mouse moves
-	glfwSetScrollCallback(window, scroll_callback); // calls scroll_callback every time scrolling happens
-	glfwSetMouseButtonCallback(window, mouse_button_callback);
 	glEnable(GL_DEPTH_TEST); // turn on z buffering
-	// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
-	// glEnable(GL_CULL_FACE); // face culling only renders visible faces of closed shapes ie. cube (needs speed testing to determine if worth)
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	// build and compile our shader program
-	Shader cubeShader("../resources/shaders/cube.vs", "../resources/shaders/cube.fs");
-	unsigned int atlas = textureEngine.loadTexture("../resources/textures/myatlas.jpg");
-
-	cubeShader.use();
-	cubeShader.setInt("atlas", 0);
 
 	// render loop
 	while (!glfwWindowShouldClose(window))
@@ -67,67 +42,9 @@ int main(void)
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		// input
-		player.processInput(window, deltaTime);
-
 		// render
 		glClearColor(0.5f, 0.7f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-		// texture atlas binding
-		glActiveTexture(GL_TEXTURE0);
-		glBindTexture(GL_TEXTURE_2D, atlas);
-
-		// for physics
-		player.update(deltaTime);
-		// setup renderer
-		cubeShader.use();
-		glm::mat4 projection = glm::perspective(glm::radians(player.camera.Zoom), (float)WIDTH / (float)HEIGHT, 0.1f, 1000.0f);
-		glm::mat4 view = player.camera.GetViewMatrix();
-		cubeShader.setMat4("projection", projection);
-		cubeShader.setMat4("view", view);
-
-		Chunk *c = player.getChunk();
-		terr.renderChunk(glm::ivec2(c->getXOff(), c->getZOff()), cubeShader);
-		// need to make sure to only render each chunk once per frame
-		for (int i = 0; i < RENDER_RADIUS; i++)
-		{
-			for (int j = 0; j < RENDER_RADIUS; j++)
-			{
-				if (!i && !j)
-					continue;
-				if (!terr.renderChunk(glm::ivec2(c->getXOff() + i, c->getZOff() + j), cubeShader))
-					break ;
-				if (!terr.renderChunk(glm::ivec2(c->getXOff() - i, c->getZOff() - j), cubeShader))
-					break ;
-				if (!terr.renderChunk(glm::ivec2(c->getXOff() - i, c->getZOff() + j), cubeShader))
-					break ;
-				if (!terr.renderChunk(glm::ivec2(c->getXOff() + i, c->getZOff() - j), cubeShader))
-					break ;
-			}
-		}
-		terr.renderWaterChunk(glm::ivec2(c->getXOff(), c->getZOff()), cubeShader);
-		for (int i = 0; i < RENDER_RADIUS; i++)
-		{
-			for (int j = 0; j < RENDER_RADIUS; j++)
-			{
-				if (!i && !j)
-					continue;
-				if (!terr.renderWaterChunk(glm::ivec2(c->getXOff() + i, c->getZOff() + j), cubeShader))
-					break ;
-				if (!terr.renderWaterChunk(glm::ivec2(c->getXOff() - i, c->getZOff() - j), cubeShader))
-					break ;
-				if (!terr.renderWaterChunk(glm::ivec2(c->getXOff() - i, c->getZOff() + j), cubeShader))
-					break ;
-				if (!terr.renderWaterChunk(glm::ivec2(c->getXOff() + i, c->getZOff() - j), cubeShader))
-					break ;
-			}
-		}
-		if (terr.updateList != glm::ivec2(-100000,-100000)) // could switch to running this as a while loop on a list on a seperate thread
-		{
-			terr.updateChunk(terr.updateList);
-			terr.updateList = glm::ivec2(-100000,-100000);
-		}
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		glfwSwapBuffers(window);
@@ -136,48 +53,6 @@ int main(void)
 	// glfw: terminate, clearing all previously allocated GLFW resources
 	glfwTerminate();
 	return 0;
-}
-
-
-
-
-
-
-
-
-
-// calculate mouse movement
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-	lastX = xpos;
-	lastY = ypos;
-
-	player.camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
-{
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-		player.leftMouseClickEvent();
-
-	if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS)
-		player.rightMouseClickEvent();
-
-}
-
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	player.camera.ProcessMouseScroll(yoffset);
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function execute
--- srcs/engine.h
+++ srcs/engine.h
@@ -14,9 +14,6 @@
 #include <glm/glm.hpp> // vec3, vec4, ivec4, mat4
 #include <glm/gtc/matrix_transform.hpp> // translate, rotate, scale, perspective 
 #include <glm/gtc/type_ptr.hpp> // value_ptr
-#define GLM_ENABLE_EXPERIMENTAL
-#include <glm/gtx/hash.hpp> // for unordered_map
-#include <unordered_map>
 #include <cmath>
 #include <thread>
 #include <string>
@@ -33,12 +30,7 @@
 #define TEXTURE_SIZE 16
 #define DIST(X,Y,XX,YY) ((YY-Y)/(XX-X));
 
-#include "blockIndex.h" // block types
-#include "cubeMap.h"
-
 using namespace std; // should move to cpp files for locality
-
-#include "chunk.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -48,6 +40,4 @@ unsigned int loadTexture(char const * path);
 unsigned int TextureFromFile(const char *path, const string &directory);
 unsigned int skybox(void);
 unsigned int loadCubemap(vector<std::string> faces);
-float noise(float x, float y);
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
-void diamondSquare(int Array[CHUNK_X][CHUNK_Z], int size);

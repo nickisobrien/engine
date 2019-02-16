da2af20c8c872e4a7fc54a43e14fcf0cb005b129
Nick <hoop.bbb@gmail.com>
2019-02-16 11:10:38 -0800

    switched block type to uint8_t and made variables inline

--- includes/block.hpp
+++ includes/block.hpp
@@ -4,11 +4,11 @@ class Block
 {
 public:
 	inline Block() {};
-	~Block();
+	inline ~Block() {};
 
-	bool isActive(void);
-	int getType(void);
-	void setType(int t);
+	inline bool isActive(void) { return (this->type == Blocktype::AIR_BLOCK || this->type == Blocktype::WATER_BLOCK) }
+	inline uint8_t getType(void) { return this->type; }
+	inline void setType(uint8_t t) { this->type = t; }
 private:
 	uint8_t type = AIR_BLOCK; //need to change to smaller data size ie. char/short// 1==grass, 0==air, 2==sand, 3==snow 
 };
